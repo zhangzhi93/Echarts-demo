51ca54275381d5a0e2b97b18016875245e1229f6
zhangzhi93 <zhangzhi93@hotmail.com>
2018-09-25 07:58:53 +0800

    linechart

--- src/js/util.js
+++ src/js/util.js
@@ -1,11 +1,11 @@
 
 export default {
   Base: {
-    contextPath: "/api/",
+    //contextPath: "/api/",
     //contextPath: "http://dashiji.gtzmmf.com/",
     //contextPath: "http://user-dashiji.gtzmmf.com/",
     //contextPath: "http://47.93.193.171:7170/",
-    //contextPath: "http://139.224.8.29:19080/",
+    contextPath: "http://139.224.8.29:19080/",
     endPath: ".do",
   },
   getParaValueByName: function (paraName) {
